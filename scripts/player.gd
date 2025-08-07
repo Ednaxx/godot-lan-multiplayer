f2f62ad077beaf81f057d696148574b1aee40903
Ana Ruth Bezerra <an.bezerra@gmail.com>
2025-08-07 15:42:24 -0300

    fix: enable score system for singleplayer mode

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,13 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Para compatibilidade com o sistema de pontuação
+func get_player_id():
+	return 1
+
+func _ready():
+	add_to_group("players")
+
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
--- scripts/score_label.gd
+++ scripts/score_label.gd
@@ -3,16 +3,25 @@ extends Label
 
 func _ready():
     var gm = get_tree().get_current_scene().get_node("GameManager")
-    var player = get_tree().get_first_node_in_group("players")
+    var player = _get_local_player()
     var player_id = null
+    
     if player and player.has_method("get_player_id"):
         player_id = player.get_player_id()
+    
     if gm and player_id != null:
         gm.connect("score_updated", Callable(self, "_on_score_updated"))
         _on_score_updated(player_id, gm.get_score(player_id))
 
+func _get_local_player():
+    # Para singleplayer, pega o nó Player direto
+    if not MultiplayerManager.multiplayer_mode_enabled:
+        return get_tree().get_current_scene().get_node_or_null("Player")
+    else:
+        # Para multiplayer, pega da group "players"
+        return get_tree().get_first_node_in_group("players")
 
 func _on_score_updated(player_id, new_score):
-    var player = get_tree().get_first_node_in_group("players")
+    var player = _get_local_player()
     if player and player.has_method("get_player_id") and player.get_player_id() == player_id:
         text = str(new_score) + " Points"
